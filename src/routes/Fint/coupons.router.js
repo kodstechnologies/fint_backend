93ee2b1cff860946aa41868ccc0fee66c7b6a9c4
sangram <balsangram1@gmail.com>
2025-07-11 10:38:01 +0530

    update coupons related add authentication

--- src/routes/Fint/coupons.router.js
+++ src/routes/Fint/coupons.router.js
@@ -13,7 +13,7 @@ import {
   displayActiveCoupons
 } from "../../controllers/fintConmtroller/fintCoupon.controller.js"; // Update path as needed
 import { upload } from "../../middlewares/multer.middleware.js";
-import { ventureVerifyRefreshToken } from "../../middlewares/auth.venture.middleware.js";
+import { ventureVentureverifyJWT } from "../../middlewares/auth.venture.middleware.js";
 import { userverifyJWT } from "../../middlewares/auth.user.middleware.js";
 
 const router = Router();
@@ -22,26 +22,20 @@ const router = Router();
  * @route   POST /create
  * @desc    Create (sign up) a new Fint user
  */
-router.post("/create",ventureVerifyRefreshToken,upload.single("logo"), createCoupon);
+
 router.get("/display-all-coupons", displayCoupons);
 
-router.get("/deleted-coupons",ventureVerifyRefreshToken, displayDeletedCoupons);
 
 
-/**
- * @route   GET /coupons/user/:id
- * @desc    Get coupons for a specific user
-*/
-// router.get("/coupons/user/:id", getUserCouponsById);
-
-/**
- * @route   GET /coupons/venture/:id
- * @desc    Get coupons for a specific venture
-*/
-router.get("/venture/:id", getVentureCouponsById);
-router.get("/expired-coupons/:id", displayVentureExpiredCoupons);
+// fint venture coupons related apis 
+router.post("/create",ventureVentureverifyJWT,upload.single("logo"), createCoupon);
+router.get("/deleted-coupons",ventureVentureverifyJWT, displayDeletedCoupons);
+router.get("/venture/:id",ventureVentureverifyJWT, getVentureCouponsById);
+router.get("/expired-coupons/:id", ventureVentureverifyJWT,displayVentureExpiredCoupons);
+router.get("/ventue-display-all-coupons",ventureVentureverifyJWT, displayCoupons);
 
 
+// fint user coupons related apis  
 router.get("/active-coupons",userverifyJWT, displayActiveCoupons);
 router.get("/expired-coupons",userverifyJWT, displayExpiredCoupons);
 router.get("/display-coupons-details/:id",userverifyJWT, displayCouponDetails);
