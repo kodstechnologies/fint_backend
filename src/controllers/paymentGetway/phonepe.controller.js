bca5102d06fde0d9f7b1d6f4d0fe55c9834ba820
sangram <balsangram1@gmail.com>
2025-08-14 10:24:48 +0530

    updated code

--- src/controllers/paymentGetway/phonepe.controller.js
+++ src/controllers/paymentGetway/phonepe.controller.js
@@ -1,37 +1,37 @@
-// import axios from "axios";
-// import { asyncHandler } from "../../utils/asyncHandler.js";
-// import Payment from "../../models/payment/payment.model.js";
-// import { ApiResponse } from "../../utils/ApiResponse.js";
-// import { ApiError } from "../../utils/ApiError.js";
-// // import { generateXVerify } from "../../utils/phonepe.helper.js";
+import axios from "axios";
+import { asyncHandler } from "../../utils/asyncHandler.js";
+import Payment from "../../models/payment/payment.model.js";
+import { ApiResponse } from "../../utils/ApiResponse.js";
+import { ApiError } from "../../utils/ApiError.js";
+// import { generateXVerify } from "../../utils/phonepe.helper.j
 
+// export const creater 
 
+export const checkPaymentStatus = async (req, res) => {
+  try {
+    const { transactionId } = req.params;
 
-// export const checkPaymentStatus = async (req, res) => {
-//   try {
-//     const { transactionId } = req.params;
+    const relativeUrl = `/pg/v1/status/${process.env.MERCHANT_ID}/${transactionId}`;
 
-//     const relativeUrl = `/pg/v1/status/${process.env.MERCHANT_ID}/${transactionId}`;
+    const xVerify = generateXVerify("", relativeUrl);
 
-//     const xVerify = generateXVerify("", relativeUrl);
+    const response = await axios.get(
+      `${process.env.PHONEPE_BASE_URL}${relativeUrl}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-VERIFY": xVerify,
+          "X-MERCHANT-ID": process.env.MERCHANT_ID,
+        },
+      }
+    );
 
-//     const response = await axios.get(
-//       `${process.env.PHONEPE_BASE_URL}${relativeUrl}`,
-//       {
-//         headers: {
-//           "Content-Type": "application/json",
-//           "X-VERIFY": xVerify,
-//           "X-MERCHANT-ID": process.env.MERCHANT_ID,
-//         },
-//       }
-//     );
-
-//     return res.status(200).json({
-//       success: true,
-//       data: response.data,
-//     });
-//   } catch (error) {
-//     console.error(error.response?.data || error.message);
-//     res.status(500).json({ error: "Failed to fetch status" });
-//   }
-// };
+    return res.status(200).json({
+      success: true,
+      data: response.data,
+    });
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).json({ error: "Failed to fetch status" });
+  }
+};
