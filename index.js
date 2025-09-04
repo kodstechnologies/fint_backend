bbdbea33df99a065e7d5b5c5c0a3384482f93ee9
sangram <balsangram1@gmail.com>
2025-09-05 03:03:32 +0530

    update 9

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use('/', mainRouter);
 // Test route
 
 app.get("/test", (req, res) => {
-  res.send("Backend is working! ...5");
+  res.send("Backend is working! ...5-9");
 });
 // Error handler middleware
 app.use(errorHandler);
