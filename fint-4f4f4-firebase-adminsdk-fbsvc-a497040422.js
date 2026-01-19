046244db228ae279f1925b64470ba9052d714acb
sangram <balsangram1@gmail.com>
2026-01-19 17:13:43 +0530

    firebase code comment out

--- fint-4f4f4-firebase-adminsdk-fbsvc-a497040422.js
+++ fint-4f4f4-firebase-adminsdk-fbsvc-a497040422.js
@@ -1,27 +1,27 @@
-import config from "./src/config/index.js";
-const { FIREBASE_TYPE, FIREBASE_PROJECT_ID, FIREBASE_PRIVATE_KEY_ID, FIREBASE_PRIVATE_KEY, FIREBASE_CLIENT_EMAIL, FIREBASE_CLIENT_ID, FIREBASE_AUTH_URI, FIREBASE_TOKEN_URI, FIREBASE_AUTH_PROVIDER_X509_CERT_URL, FIREBASE_CLIENT_X509_CERT_URL, FIREBASE_UNIVERSE_DOMAIN } = config;
+// import config from "./src/config/index.js";
+// const { FIREBASE_TYPE, FIREBASE_PROJECT_ID, FIREBASE_PRIVATE_KEY_ID, FIREBASE_PRIVATE_KEY, FIREBASE_CLIENT_EMAIL, FIREBASE_CLIENT_ID, FIREBASE_AUTH_URI, FIREBASE_TOKEN_URI, FIREBASE_AUTH_PROVIDER_X509_CERT_URL, FIREBASE_CLIENT_X509_CERT_URL, FIREBASE_UNIVERSE_DOMAIN } = config;
 
 export const userServiceAccount = {
-  // type: "service_account",
-  // project_id: "fint-4f4f4",
-  // private_key_id: "a497040422f2d9348cc2f317ace05ac7cdeb4246",
-  // private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDgSuFfOwqymwhR\nPwr0WAcH7KrRG1N2x7aEQXk1WG6Lt54KWyoy651HLQibFM7zOo5MBlQ0dH675SUz\nkpFzli64VdvYpmKvnxLqHgIGI9S4Rsr85MwxmBfytn2WPAfktg/WJ15Hfz043oeS\nGRizY7ZsntgqnRgmYlXeIM4Uf4X9dny11UbrHotMaWTHuni+gxBkFmfKMqA7A7AJ\nyslO80wyEhraoLk2Qz5eb4NbhRCc2QrUCaDr4mDTHWzPg1bIK5XK8AanYtyWbA4X\ncINDIDef6Zb9jH0w1PgsAAOvR9R3MCq9Umq8w/wm9mr/WNjMHvRZ5qaAWPJAjD2v\nJgez7z6xAgMBAAECggEALqrl4a6jNy5SuLTpTpXSQK/ZitCouRosu6/zzEmuFpwJ\n4hW93gJwIxMXq0zVEn6ohOD8PD3Q8L9o0iY45VfmOKyEpbtY5Tmv8c50lVp3NDBm\no9/nLJp8wR9/Wfb7nXbULLbcQvwSfPawNpHKNekVV9xlFdjB4ZqsSx+AQXuXIXlW\nET1DzWjB1ZcI3gtbH1IZcFFGKCF6QOAJDZJW4O3+sKG+yPnC4P6sG+kSt6Wpw97Z\nsqi+u+XGEUlH7sp+IqWLyTM0n3ZnbPU+V1QBdkKt/bBQAmBNlA7b2umiFNYzSK0y\nG6VFULjM6C8zzQoOGtg0dYoTKa9DHlnnnQXvX6mg0QKBgQD2Cw3+qas6Ru2+DeDk\n1nfutbVpbTSbDkXiD7kbGHXVL8+aGy7KRAv42z/CGYImMLsrygNSEy5SHgVYV2l2\nPwKaOh8ITfalSAGlsqepqPWOvzeEkyK+7kRRXi09ZJKHlrC07t+bvUvH7CUwSjUS\noTNVIW8CeUL9EVcO21s5gY36EwKBgQDpXn5+zL3mff6lgRLahVhj8enryc/xJ73+\nYhZnL7cR9M8+FlHyDGUiUcBqDwO3Gnn72PEHpODHAk3mgmswHY3ws03Xxnq69d2e\nAENQz6HBk8E1lby5cne4t/FR2QIyFshmi9m/ClGTDoJN7W0ohpGFR6oEOrSxt+/o\ne60ipih8qwKBgQC5vBSfdvO0dZDJFO79sM0unWtKH89Dcd3dTmM9ChUBjysXuYdz\n1zR2cwRJgToXnFffMYuaLLKd+N2uF6acReqwBEoi2b/ke1wK+rfIwqxESyMc6Hml\nSna2OfLK0p4uztmsbFrO1XJchG56arpvytLWY/vKEUI5ihp/M6ZE3gEQkwKBgQCT\nxr6bVNM7zbUe4KkrrxzVzS6vnNAMiXoKR9qf5Nssh7VkP4YMnES+gr4xauiTXjS4\nghu/F20vGAp0WUm+4jgrn58/b/PvyBlaIBiAZ7qqdgH3CKbFV/LwCarKhXyac0rb\nhNuR2drmIF3rMtnYu74Sg9rparNZhnxo30g3lLg2mQKBgGxeddKAA+K3Wi+b/4ZP\nm005Ydpu7pmUUxGM4qSUUialgwtKdp8XU2qZ5v2sceA32PKWnkxcEUEsTouhncs8\nXoV9RWoGxjkmBst7dyIMEVx//t4rjrvR9Bk/VcV1Gtf3uHYTIqsb9jVBxOkR9KnY\n167VaRiwQD2beoHamOLdhJeF\n-----END PRIVATE KEY-----\n",
-  // client_email: "firebase-adminsdk-fbsvc@fint-4f4f4.iam.gserviceaccount.com",
-  // client_id: "114210402993257298989",
-  // auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  // token_uri: "https://oauth2.googleapis.com/token",
-  // auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  // client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40fint-4f4f4.iam.gserviceaccount.com",
-  // universe_domain: "googleapis.com"
-  type: FIREBASE_TYPE,
-  project_id: FIREBASE_PROJECT_ID,
-  private_key_id: FIREBASE_PRIVATE_KEY_ID,
-  private_key: FIREBASE_PRIVATE_KEY,
-  client_email: FIREBASE_CLIENT_EMAIL,
-  client_id: FIREBASE_CLIENT_ID,
-  auth_uri: FIREBASE_AUTH_URI,
-  token_uri: FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: FIREBASE_CLIENT_X509_CERT_URL,
-  universe_domain: FIREBASE_UNIVERSE_DOMAIN
+  type: "service_account",
+  project_id: "fint-4f4f4",
+  private_key_id: "a497040422f2d9348cc2f317ace05ac7cdeb4246",
+  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDgSuFfOwqymwhR\nPwr0WAcH7KrRG1N2x7aEQXk1WG6Lt54KWyoy651HLQibFM7zOo5MBlQ0dH675SUz\nkpFzli64VdvYpmKvnxLqHgIGI9S4Rsr85MwxmBfytn2WPAfktg/WJ15Hfz043oeS\nGRizY7ZsntgqnRgmYlXeIM4Uf4X9dny11UbrHotMaWTHuni+gxBkFmfKMqA7A7AJ\nyslO80wyEhraoLk2Qz5eb4NbhRCc2QrUCaDr4mDTHWzPg1bIK5XK8AanYtyWbA4X\ncINDIDef6Zb9jH0w1PgsAAOvR9R3MCq9Umq8w/wm9mr/WNjMHvRZ5qaAWPJAjD2v\nJgez7z6xAgMBAAECggEALqrl4a6jNy5SuLTpTpXSQK/ZitCouRosu6/zzEmuFpwJ\n4hW93gJwIxMXq0zVEn6ohOD8PD3Q8L9o0iY45VfmOKyEpbtY5Tmv8c50lVp3NDBm\no9/nLJp8wR9/Wfb7nXbULLbcQvwSfPawNpHKNekVV9xlFdjB4ZqsSx+AQXuXIXlW\nET1DzWjB1ZcI3gtbH1IZcFFGKCF6QOAJDZJW4O3+sKG+yPnC4P6sG+kSt6Wpw97Z\nsqi+u+XGEUlH7sp+IqWLyTM0n3ZnbPU+V1QBdkKt/bBQAmBNlA7b2umiFNYzSK0y\nG6VFULjM6C8zzQoOGtg0dYoTKa9DHlnnnQXvX6mg0QKBgQD2Cw3+qas6Ru2+DeDk\n1nfutbVpbTSbDkXiD7kbGHXVL8+aGy7KRAv42z/CGYImMLsrygNSEy5SHgVYV2l2\nPwKaOh8ITfalSAGlsqepqPWOvzeEkyK+7kRRXi09ZJKHlrC07t+bvUvH7CUwSjUS\noTNVIW8CeUL9EVcO21s5gY36EwKBgQDpXn5+zL3mff6lgRLahVhj8enryc/xJ73+\nYhZnL7cR9M8+FlHyDGUiUcBqDwO3Gnn72PEHpODHAk3mgmswHY3ws03Xxnq69d2e\nAENQz6HBk8E1lby5cne4t/FR2QIyFshmi9m/ClGTDoJN7W0ohpGFR6oEOrSxt+/o\ne60ipih8qwKBgQC5vBSfdvO0dZDJFO79sM0unWtKH89Dcd3dTmM9ChUBjysXuYdz\n1zR2cwRJgToXnFffMYuaLLKd+N2uF6acReqwBEoi2b/ke1wK+rfIwqxESyMc6Hml\nSna2OfLK0p4uztmsbFrO1XJchG56arpvytLWY/vKEUI5ihp/M6ZE3gEQkwKBgQCT\nxr6bVNM7zbUe4KkrrxzVzS6vnNAMiXoKR9qf5Nssh7VkP4YMnES+gr4xauiTXjS4\nghu/F20vGAp0WUm+4jgrn58/b/PvyBlaIBiAZ7qqdgH3CKbFV/LwCarKhXyac0rb\nhNuR2drmIF3rMtnYu74Sg9rparNZhnxo30g3lLg2mQKBgGxeddKAA+K3Wi+b/4ZP\nm005Ydpu7pmUUxGM4qSUUialgwtKdp8XU2qZ5v2sceA32PKWnkxcEUEsTouhncs8\nXoV9RWoGxjkmBst7dyIMEVx//t4rjrvR9Bk/VcV1Gtf3uHYTIqsb9jVBxOkR9KnY\n167VaRiwQD2beoHamOLdhJeF\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-fbsvc@fint-4f4f4.iam.gserviceaccount.com",
+  client_id: "114210402993257298989",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40fint-4f4f4.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com"
+  // type: FIREBASE_TYPE,
+  // project_id: FIREBASE_PROJECT_ID,
+  // private_key_id: FIREBASE_PRIVATE_KEY_ID,
+  // private_key: FIREBASE_PRIVATE_KEY,
+  // client_email: FIREBASE_CLIENT_EMAIL,
+  // client_id: FIREBASE_CLIENT_ID,
+  // auth_uri: FIREBASE_AUTH_URI,
+  // token_uri: FIREBASE_TOKEN_URI,
+  // auth_provider_x509_cert_url: FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  // client_x509_cert_url: FIREBASE_CLIENT_X509_CERT_URL,
+  // universe_domain: FIREBASE_UNIVERSE_DOMAIN
 }
